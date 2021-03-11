c3f754f79fce1719ce7ee37edf4e8a37c1dbdc67
Ervizal Buana <ervizal14@gmail.com>
2021-03-11 21:04:40 +0700

    Pengerjaan Exercise

---
+++ I0320035_exercise2.1.py
@@ -0,0 +1,19 @@
+# Tipe data boolean
+print(True)
+
+# Tipe data string
+print("Ayo belajar python")
+print('Belajar python itu mudah')
+
+# Tipe data integer
+print(20)
+
+# Tipe data float
+print(3.14)
+
+# Tipe data hexadecimal
+hex(p) = 9a
+print(p)
+
+# Tipe data complex
+print(5j)
